233bf455ee24a302144947db617d4e7a03766bd6
Itzkirimi <kirimihansel@gmail.com>
2024-01-15 00:52:15 +0300

    Done with Assignments

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+
+newHeader.innerHTML = "Hansel is the champion";
+
+
+document.body.appendChild(newHeader);
